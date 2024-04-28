bf0808355e863f4f91a9903fb13f64b1d837a7bb
Jia Xiong <wing8169@users.noreply.github.com>
2024-04-28 22:49:29 +0800

    Init

---
+++ run.sh
@@ -0,0 +1,14 @@
+cd workspace
+# install nodejs and npm
+apt install -y nodejs
+apt install -y npm
+# install pm2
+npm install pm2 -g
+# install ollama
+curl -fsSL https://ollama.com/install.sh
+sh install.sh
+wget https://huggingface.co/TheBloke/Silicon-Maid-7B-GGUF/resolve/main/silicon-maid-7b.Q8_0.gguf
+wget https://green-mad-elk-149.mypinata.cloud/ipfs/QmavQtTprdaogQMVpYz7735Ve5zumjANpc6JgXHgnRacFm/siliconmaid-7b-seraphina/Modelfile
+ollama create siliconmaid -f Modelfile
+# start pm2
+pm2 start main.py
