fd59dcff035a167de30058b973fa220ae2c7e19f
Jia Xiong <wing8169@users.noreply.github.com>
2024-04-29 00:05:05 +0800

    Adjust

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-cd workspace
 # install nodejs and npm
 apt install -y nodejs
 apt install -y npm
@@ -9,6 +8,6 @@ wget https://ollama.com/install.sh
 sh install.sh
 wget https://huggingface.co/TheBloke/Silicon-Maid-7B-GGUF/resolve/main/silicon-maid-7b.Q8_0.gguf
 wget https://green-mad-elk-149.mypinata.cloud/ipfs/QmavQtTprdaogQMVpYz7735Ve5zumjANpc6JgXHgnRacFm/siliconmaid-7b-seraphina/Modelfile
-ollama create siliconmaid -f Modelfile
 # start pm2
 pm2 start main.py
+ollama create siliconmaid -f Modelfile
