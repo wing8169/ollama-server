dcd600cb0db73ec2ea4147d3bd8c5c28d0c07704
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-05-04 15:28:49 +0800

    Add llama3 script

---
+++ run_llama3.sh
@@ -0,0 +1,14 @@
+# install nodejs and npm
+curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
+bash /tmp/nodesource_setup.sh
+apt-get install nodejs -y
+# install pm2
+npm install pm2 -g
+# install ollama
+wget https://ollama.com/install.sh
+sh install.sh
+wget https://huggingface.co/MaziyarPanahi/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q8_0.gguf
+echo "FROM ./Meta-Llama-3-8B-Instruct.Q8_0.gguf" > Modelfile
+# start pm2
+pm2 start main.py
+ollama create llama3 -f Modelfile
